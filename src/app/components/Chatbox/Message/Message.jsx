e38235128fa9db2876dae7700ada839d44085e4e
cd10012 <ced361@nyu.edu>
2017-11-07 23:22:50 -0500

    Message view update
    -added timestamp
    -added profile image
    -changed styling, html structure

--- src/app/components/Chatbox/Message/Message.jsx
+++ src/app/components/Chatbox/Message/Message.jsx
@@ -2,14 +2,25 @@ import React, { Component, PropTypes } from 'react';
 import './Message.css';
 import ReactCSSTransitionReplace from 'react-css-transition-replace';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import Timestamp from 'react-timestamp';
 
 export default class Message extends Component {
   render(props) {
     return (
       <div className="message-container">
-        <h2 className="author">{this.props.author}</h2>
-            <p className="message">:      {this.props.children}</p>
+        <div className="profile-img-c">
+          <div className="profile-img" ></div>
+        </div>
+        <div className="detail-c">
+          <div className="top-c">
+            <h2 className="author">{this.props.author}</h2>
+            <p className="timestamp"><Timestamp time={this.created_at} format='time' /></p>
+          </div>
+            <p className="title">{this.props.role}</p>
+          <p className="message">:      {this.props.children}</p>
+        </div>
       </div>
+
     );
   }
 }
